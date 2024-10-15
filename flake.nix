b9244193358126e0d49be66f71420f7c0a9d3e8c
kuviman <kuviman@gmail.com>
2024-10-15 21:43:29 +0400

    poetry2nix (but doesnt work)

--- flake.nix
+++ flake.nix
@@ -2,16 +2,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    poetry2nix-flake.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { nixpkgs, systems, self }:
+  outputs = { nixpkgs, systems, poetry2nix-flake, self }:
     let
       pkgsFor = system: import nixpkgs { inherit system; };
       forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system:
-        let pkgs = pkgsFor system;
-        in f { inherit system pkgs; });
+        let
+          pkgs = pkgsFor system;
+          poetry2nix = import poetry2nix-flake { inherit pkgs; };
+        in
+        f { inherit system pkgs poetry2nix; });
     in
     {
+      # Doesnt work: https://github.com/nix-community/poetry2nix/issues/555
+      packages = forEachSystem ({ poetry2nix, ... }: {
+        default = poetry2nix.mkPoetryApplication {
+          projectDir = self;
+        };
+      });
       devShells = forEachSystem ({ pkgs, ... }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
