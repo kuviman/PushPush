d961de4b2d657a05fda4f82820ab2e3c49cb4c7e
kuviman <kuviman@gmail.com>
2024-10-15 21:11:52 +0400

    Relicense to MIT

--- makerelease.py
+++ makerelease.py
@@ -1,23 +1,3 @@
-
-##    PushPush v1.0
-##
-##    Copyright (C) 2011 kuviman
-##
-##    This program is free software: you can redistribute it and/or modify
-##    it under the terms of the GNU General Public License as published by
-##    the Free Software Foundation, either version 3 of the License, or
-##    (at your option) any later version.
-##
-##    This program is distributed in the hope that it will be useful,
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##    GNU General Public License for more details.
-##
-##    You should have received a copy of the GNU General Public License
-##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##
-##    you can contact me kuviman@gmail.com
-
 from ppgame.constants import *
 
 script = 'PushPush.py'
@@ -26,28 +6,6 @@ exe_path = '%s_%s_win32'%(TITLE, VERSION)
 data = ['data', 'levels']
 
 pythondll = 'C:/python26/python26.dll'
-
-gplcomment = '''
-##    %s
-##
-##    %s
-##
-##    This program is free software: you can redistribute it and/or modify
-##    it under the terms of the GNU General Public License as published by
-##    the Free Software Foundation, either version 3 of the License, or
-##    (at your option) any later version.
-##
-##    This program is distributed in the hope that it will be useful,
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##    GNU General Public License for more details.
-##
-##    You should have received a copy of the GNU General Public License
-##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##
-##    you can contact me kuviman@gmail.com
-
-''' % (FULL_TITLE, COPYRIGHT)
 
 import py2exe, shutil, sys, os
 from  distutils.core import setup
@@ -63,7 +21,6 @@ for root, dirs, files in os.walk(srcpath):
         if os.path.splitext(name)[1] == '.py':
             text = open(path).read()
             f = open(path, 'w')
-            f.write(gplcomment)
             f.write(text)
             f.close()
         z.write(path)
--- ppgame/main.py
+++ ppgame/main.py
@@ -1,23 +1,3 @@
-
-##    PushPush v1.0
-##
-##    Copyright (C) 2011 kuviman
-##
-##    This program is free software: you can redistribute it and/or modify
-##    it under the terms of the GNU General Public License as published by
-##    the Free Software Foundation, either version 3 of the License, or
-##    (at your option) any later version.
-##
-##    This program is distributed in the hope that it will be useful,
-##    but WITHOUT ANY WARRANTY; without even the implied warranty of
-##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##    GNU General Public License for more details.
-##
-##    You should have received a copy of the GNU General Public License
-##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##
-##    you can contact me kuviman@gmail.com
-
 import webbrowser
 import ke
 from .settings import *
@@ -119,7 +99,6 @@ def run():
         raise ke.shutdown
     am.add_label("%s game"%TITLE)
     am.add_label("version %s"%VERSION)
-    am.add_label("released under GNU GPLv3")
     am.add_label(COPYRIGHT)
     am.add_separator()
     am.add_button("visit homepage", visit)
